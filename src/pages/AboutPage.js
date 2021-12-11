1a1babf4444881f7b1ca1a258bcfdce8a34cf336
sreyasqb <20pt33@psgtech.ac.in>
2021-12-11 13:44:15 +0530

    mousetrail created

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,40 @@
+import classes from './AboutPage.module.css';
+const AboutPage = (props) => {
+  return (
+    <div className={classes.content}>
+      <span>H</span>
+      <span>e</span>
+      <span>y</span>
+      <span>,&nbsp;</span>
+      <span>I'</span>
+      <span>m&nbsp;</span>
+      <span>S</span>
+      <span>r</span>
+      <span>e</span>
+      <span>y</span>
+      <span>a</span>
+      <span>s</span>
+      <p style={{ paddingTop: "5vh" }} />
+      <p>
+        I'm a Fullstack Web & App Developer from Bangalore, India. I have a
+        passion for designing and developing websites and applications.
+      </p>
+      <br />
+      <p>
+        I love programming and problem solving, Self taught and still
+        learning!!! Fortunate and lucky for where I am in life.
+      </p>
+      <br />
+      <p>
+        Fond of interacting with different people, I learn a lot from the people
+        around me. Love sports and esports!
+      </p>
+      <br />
+      <p>
+        Interested in working and collaborating with people in creative projects
+        related to fullstack development
+      </p>
+    </div>
+  );
+};
+export default AboutPage;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,16 +2,83 @@ import React from "react";
 import SideBar from "../components/SideBar";
 import classes from "./HomePage.module.css";
 const HomePage = (props) => {
+  var dots = [],
+    mouse = {
+      x: 0,
+      y: 0,
+    };
+
+  // The Dot object used to scaffold the dots
+  var Dot = function () {
+    this.x = 0;
+    this.y = 0;
+    
+    this.node = (function () {
+      var n = document.createElement("div");
+      n.className = classes.trail;
+      document.body.prepend(n);
+      return n;
+    })();
+  };
+  // The Dot.prototype.draw() method sets the position of
+  // the object's <div> node
+  Dot.prototype.draw = function () {
+    this.node.style.left = this.x + "px";
+    this.node.style.top = this.y + "px";
+  };
+
+  // Creates the Dot objects, populates the dots array
+  for (var i = 0; i < 30; i++) {
+    var d = new Dot();
+    dots.push(d);
+  }
+
+  // This is the screen redraw function
+  function draw() {
+    // Make sure the mouse position is set everytime
+    // draw() is called.
+    var x = mouse.x+150,
+      y = mouse.y-150;
+
+    // This loop is where all the 90s magic happens
+    dots.forEach(function (dot, index, dots) {
+      var nextDot = dots[index + 1] || dots[0];
+
+      dot.x = x;
+      dot.y = y;
+      dot.draw();
+      x += (nextDot.x - dot.x) * 0.6;
+      y += (nextDot.y - dot.y) * 0.6;
+    });
+  }
+
+  document.addEventListener("mousemove", function (event) {
+    //event.preventDefault();
+    mouse.x = event.pageX;
+    mouse.y = event.pageY;
+  });
+
+  // animate() calls draw() then recursively calls itself
+  // everytime the screen repaints via requestAnimationFrame().
+  function animate() {
+    draw();
+    requestAnimationFrame(animate);
+  }
+
+  // And get it started by calling animate().
+  animate();
+
   return (
     <div className={classes.homepage}>
       <SideBar />
       <div className={classes.content}>
-        
         <span>H</span>
         <span>e</span>
         <span>y</span>
         <span>,&nbsp;</span>
-        <span>I'</span>
+        <br />
+        <span>I</span>
+        <span>'</span>
         <span>m&nbsp;</span>
         <span>S</span>
         <span>r</span>
@@ -19,26 +86,26 @@ const HomePage = (props) => {
         <span>y</span>
         <span>a</span>
         <span>s</span>
-        <p style={{paddingTop:"5vh"}}/>
-        <p>
-          I'm a Fullstack Web & App Developer from Bangalore, India. I have a
-          passion for designing and developing websites and applications.
-        </p>
+        <span>,</span>
         <br />
-        <p>
-          I love programming and problem solving, Self taught and still
-          learning!!! Fortunate and lucky for where I am in life.
-        </p>
-        <br />
-        <p>
-          Fond of interacting with different people, I learn a lot from the
-          people around me. Love sports and esports!
-        </p>
-        <br />
-        <p>
-          Interested in working and collaborating with people in creative
-          projects related to fullstack development
-        </p>
+        <span>F</span>
+        <span>u</span>
+        <span>l</span>
+        <span>l</span>
+        <span>s</span>
+        <span>t</span>
+        <span>a</span>
+        <span>c</span>
+        <span>k&nbsp;</span>
+        <span>d</span>
+        <span>e</span>
+        <span>v</span>
+        <span>e</span>
+        <span>l</span>
+        <span>o</span>
+        <span>p</span>
+        <span>e</span>
+        <span>r</span>
       </div>
     </div>
   );
